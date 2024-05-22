f4aef63723c00d6f7af32c5d1ef736f5bf7c510c
Christopher Chiarandini <chiarandini.christopher@gmail.com>
2024-05-22 11:14:07 +0200

    test 2

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,6 +11,8 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Hello World!!</h1>
+
+      <p>Je ssuis une mite en pull over</p>
     </div>
   );
 }
